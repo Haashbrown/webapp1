1a4c9680d8ee42375c770be1ab48e3ede2594b05
Gab <gpaderna@addu.edu.ph>
2021-01-27 14:37:47 +0800

    demo on modules

---
+++ app.js
@@ -0,0 +1,21 @@
+//const mymodule = require('./mymodule');
+const { clickMe } = require('./mymodule');
+
+clickMe();
+//console.log(mymodule)
+//mymodule.clickMe()
+
+//const os = require('os');
+const fs = require('fs');
+
+//fs.readFile()
+//fs.createReadStream();
+
+/*fs.readFile('./lorenz.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data.toString());
+    }
+});
+console.log('last line');*/
---
+++ mymodule.js
@@ -0,0 +1,5 @@
+const fname = ["Dodong"];
+const clickMe  = function (firstname = null) {
+    console.log(`Hello ${firstname}`);
+}
+module.exports = { fname, clickMe };
